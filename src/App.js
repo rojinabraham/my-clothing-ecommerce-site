02a21008ead9b22945d5cd164a538898a846d866
Rojin Abraham <rojinabraham@gmail.com>
2020-09-01 18:39:00 +0530

    added firebase auth

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { Route, Switch } from "react-router-dom";
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+import Header from "./componnents/header/header.component";
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user.email);
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDehf8pkTGxjEUAn1dgHMYM4OmV2zBEQdc",
+  authDomain: "ecom-db-6c1eb.firebaseapp.com",
+  databaseURL: "https://ecom-db-6c1eb.firebaseio.com",
+  projectId: "ecom-db-6c1eb",
+  storageBucket: "ecom-db-6c1eb.appspot.com",
+  messagingSenderId: "398522159183",
+  appId: "1:398522159183:web:6239a6d75951a5a6811a3a",
+  measurementId: "G-6F5N91TLZ0",
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//gets access to new GoogleAuthProvider class from "auth" library
+const provider = new firebase.auth.GoogleAuthProvider();
+//trigger google pop up whenever we use googleauthprov for auth and sign in
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
