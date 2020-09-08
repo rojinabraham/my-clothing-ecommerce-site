c3f906eedf56be5885256ac4e05fad4db55d55b9
Rojin Abraham <rojinabraham@gmail.com>
2020-09-08 15:37:38 +0530

    added additem and remove item fn to checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,5 +7,13 @@ const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item,
 });
+const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
 
-export { toggleCartHidden, addItem };
+export { removeItem, clearItemFromCart, toggleCartHidden, addItem };
